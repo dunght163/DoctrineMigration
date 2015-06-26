36a0e0b024fa5abef99435aa06febff8b6af55f3
dunght160387 <dunght1603@gmail.com>
2015-06-26 10:24:02 +0700

    create table log for logging

---
+++ app/DoctrineMigrations/Version20150626102213_CreateLogTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ *
+ * Create table for logging.
+ */
+class Version20150626102213_CreateLogTable extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, created_time DATE NOT NULL, ip VARCHAR(255) NOT NULL, server_ip VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, descr LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE log');
+    }
+}
---
+++ src/AppBundle/Entity/Log.php
@@ -0,0 +1,195 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Log
+ */
+class Log
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $userId;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdTime;
+
+    /**
+     * @var string
+     */
+    private $ip;
+
+    /**
+     * @var string
+     */
+    private $serverIp;
+
+    /**
+     * @var string
+     */
+    private $action;
+
+    /**
+     * @var array
+     */
+    private $descr;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     * @return Log
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return integer 
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set createdTime
+     *
+     * @param \DateTime $createdTime
+     * @return Log
+     */
+    public function setCreatedTime($createdTime)
+    {
+        $this->createdTime = $createdTime;
+
+        return $this;
+    }
+
+    /**
+     * Get createdTime
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedTime()
+    {
+        return $this->createdTime;
+    }
+
+    /**
+     * Set ip
+     *
+     * @param string $ip
+     * @return Log
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+
+        return $this;
+    }
+
+    /**
+     * Get ip
+     *
+     * @return string 
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * Set serverIp
+     *
+     * @param string $serverIp
+     * @return Log
+     */
+    public function setServerIp($serverIp)
+    {
+        $this->serverIp = $serverIp;
+
+        return $this;
+    }
+
+    /**
+     * Get serverIp
+     *
+     * @return string 
+     */
+    public function getServerIp()
+    {
+        return $this->serverIp;
+    }
+
+    /**
+     * Set action
+     *
+     * @param string $action
+     * @return Log
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * Get action
+     *
+     * @return string 
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set descr
+     *
+     * @param array $descr
+     * @return Log
+     */
+    public function setDescr($descr)
+    {
+        $this->descr = $descr;
+
+        return $this;
+    }
+
+    /**
+     * Get descr
+     *
+     * @return array 
+     */
+    public function getDescr()
+    {
+        return $this->descr;
+    }
+}
