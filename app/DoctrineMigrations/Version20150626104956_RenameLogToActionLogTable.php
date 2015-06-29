a677c6b00b9d67ac066540437cc7ed4b56b53851
dunght160387 <dunght1603@gmail.com>
2015-06-29 16:53:51 +0700

    rename table done

--- app/DoctrineMigrations/Version20150626104956_RenameLogToActionLogTable.php
+++ app/DoctrineMigrations/Version20150626104956_RenameLogToActionLogTable.php
@@ -7,6 +7,8 @@ use Doctrine\DBAL\Schema\Schema;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
+ *
+ * Rename table. This not using create/drop table. Will be modified to use RENAME TABLE sql
  */
 class Version20150626104956_RenameLogToActionLogTable extends AbstractMigration
 {
@@ -18,15 +20,15 @@ class Version20150626104956_RenameLogToActionLogTable extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE action_log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, created_time DATE NOT NULL, ip VARCHAR(255) NOT NULL, server_ip VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, descr LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        // this will be wrong by using doctrine:migrations:diff -> disable this sql
+        //$this->addSql('CREATE TABLE action_log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, created_time DATE NOT NULL, ip VARCHAR(255) NOT NULL, server_ip VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, descr LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
 
-        // modified
-        //// clone data from log to action_log
-        $this->addSql('INSERT INTO TABLE action_log (SELECT * FROM action_log)');
-
+        // modified: using rename table sql
+        $this->addSql('RENAME TABLE log TO action_log');
         // end - modified
 
-        $this->addSql('DROP TABLE log');
+        // disable drop table
+        //$this->addSql('DROP TABLE log');
     }
 
     /**
@@ -37,7 +39,14 @@ class Version20150626104956_RenameLogToActionLogTable extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, created_time DATE NOT NULL, ip VARCHAR(255) NOT NULL, server_ip VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, descr LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
-        $this->addSql('DROP TABLE action_log');
+        // this will be wrong by using doctrine:migrations:diff -> disable this sql
+        //$this->addSql('CREATE TABLE log (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, created_time DATE NOT NULL, ip VARCHAR(255) NOT NULL, server_ip VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, descr LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+
+        // modified: using rename table sql
+        $this->addSql('RENAME TABLE action_log TO log');
+        // end - modified
+
+        // disable drop table
+        //$this->addSql('DROP TABLE action_log');
     }
 }
