ae4162792c3880e654102d0ccc76ddc02a061ff9
dunght160387 <dunght163@gmail.com>
2015-08-03 09:45:00 +0700

    merget master

--- app/DoctrineMigrations/Version20150616100319_Master1.php
+++ app/DoctrineMigrations/Version20150616100319_Master1.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  *
- * The first. No change. Table 'Product' created.
+ * The first. Table 'Product' created.
  */
 class Version20150616100319_Master1 extends AbstractMigration
 {
@@ -18,7 +18,9 @@ class Version20150616100319_Master1 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price DOUBLE PRECISION NOT NULL, description LONGTEXT NOT NULL COMMENT \'(DC2Type:json_array)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     /**
@@ -27,6 +29,8 @@ class Version20150616100319_Master1 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('DROP TABLE product');
     }
 }
